1e039b81a036555e9c2a76d1b1ec79fda781c075
Zippen1337 <nat.kalinin2013@yandex.ru>
2023-10-14 12:31:51 +0300

    more solutions

---
+++ JS/8kyu/CalculatePriceExludingVAT.js
@@ -0,0 +1,5 @@
+//return price without vat
+function excludingVatPrice(price){
+    if (price === null) return -1;
+    return Math.round((price / 1.15) * 100) / 100;
+  }
---
+++ JS/8kyu/ColorGhost.js
@@ -0,0 +1,5 @@
+var Ghost = function() {
+    // your code goes here
+      var colors = ['white','yellow','purple','red']
+      this.color = colors[Math.floor(Math.random() * 4)]
+  };
